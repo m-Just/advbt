0d2fe02f7d9a01d9e76ce45fdc88b230a7a37aba
m-Just <mjust.lkc@gmail.com>
2018-08-16 12:08:37 +0800

    performance improvement

--- experiments/utilities.py
+++ experiments/utilities.py
@@ -39,7 +39,7 @@ def data_cifar10():
 
     y_train = keras.utils.to_categorical(y_train, 10)
     y_test = keras.utils.to_categorical(y_test, 10)
-    
+
     x_train = x_train.astype('float32')
     x_test = x_test.astype('float32')
     x_train /= 255
@@ -73,7 +73,7 @@ def predict(sess, x, model, x_test, batch_size = 128):
                                )
     probs = np.concatenate(probs, axis = 0)
     return probs
-        
+
 def backtracking(sess, x, y, model,  x_test, y_test, params, batch_size = 128):
     tf.set_random_seed(1822)
     set_log_level(logging.DEBUG)
@@ -87,14 +87,14 @@ def backtracking(sess, x, y, model,  x_test, y_test, params, batch_size = 128):
             x:x_test[i*batch_size:(i+1)*batch_size],
             y:y_test[i*batch_size:(i+1)*batch_size]
         }))
-    if x_test.shape[0]% batch_size != 0:    
+    if x_test.shape[0]% batch_size != 0:
         adv_imgs.append(sess.run(adv_x,feed_dict = {
                 x:x_test[num_batch*batch_size:],
                 y:y_test[num_batch*batch_size:]
             }))
-                    
+
     adv_imgs = np.concatenate(adv_imgs, axis = 0)
-        
+
     return adv_imgs
 
 def backtracking_v2(sess, x, y, model,  x_test, y_test, params, ftr, ftr_params, batch_size = 128):
@@ -107,26 +107,26 @@ def backtracking_v2(sess, x, y, model,  x_test, y_test, params, ftr, ftr_params,
         adv_imgs = backtracking(sess,x,y,model, adv_imgs, y_test, params, batch_size)
         adv_imgs = np.clip(adv_imgs, a_min = adv_imgs-eps, a_max = adv_imgs + eps)
         adv_imgs = ftr(adv_imgs,**ftr_params)
-    return adv_imgs    
+    return adv_imgs
 
 def backtracking_v3(sess, x, model, adv_imgs, y_probs, params, ftr = lambda t:t, ftr_params = {}):
     logits = model.get_logits(x)
-    labels = tf.stop_gradient(tf.constant(y_probs))
-   
+    labels = tf.constant(y_probs)
+
     smoothness = tf.image.total_variation(x)
-    
+
     loss = tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=labels)
     grad, = tf.gradients(loss, x)
     normalized_grad = tf.sign(grad)
     adv_x = x + params['eps_iter'] * normalized_grad
-    
+
     #smoothness = tf.expand_dims(tf.image.total_variation(x),axis = -1)
     #sm_grad, = tf.gradients(smoothness, adv_x)
     #sm_normalized_grad = tf.sign(sm_grad)
     #adv_x = adv_x + params['sm_iter'] * sm_normalized_grad
-    
-    
-    
+
+
+
     clip_base = tf.constant(adv_imgs)
     adv_x = tf.clip_by_value(adv_x, clip_base - params['eps'], clip_base + params['eps'])
     adv_x = tf.clip_by_value(adv_x, params['clip_min'], params['clip_max'])
@@ -134,7 +134,7 @@ def backtracking_v3(sess, x, model, adv_imgs, y_probs, params, ftr = lambda t:t,
     start_imgs = adv_imgs
     sm_vals = []
     for i in range(params['nb_iter']):
-        adv_imgs, loss_val, grad_val, logits_val, sm_val = sess.run([adv_x, loss, grad, logits, smoothness], feed_dict={x: adv_imgs})
+        adv_imgs, loss_val, grad_val, ng_val, logits_val, sm_val = sess.run([adv_x, loss, grad, normalized_grad, logits, smoothness], feed_dict={x: adv_imgs})
         adv_imgs = ftr(adv_imgs, **ftr_params)
         sm_vals.append(sm_val)
         print('Recovery iteration %d: ' % (i + 1))
@@ -143,6 +143,8 @@ def backtracking_v3(sess, x, model, adv_imgs, y_probs, params, ftr = lambda t:t,
         grad_val = np.abs(grad_val)
         print('  grad(avg)=%f, (min)=%f, (max)=%f' % \
             (np.mean(grad_val), np.min(grad_val), np.max(grad_val)))
+        print('  ngra(avg)=%f, (min)=%f, (max)=%f' % \
+            (np.mean(ng_val), np.min(ng_val), np.max(ng_val)))
         print('  sm(avg)=%f, (min)=%f, (max)=%f' % \
             (np.mean(sm_val), np.min(sm_val), np.max(sm_val)))
     return adv_imgs, sm_vals
@@ -150,7 +152,7 @@ def backtracking_v3(sess, x, model, adv_imgs, y_probs, params, ftr = lambda t:t,
 def attack_classifier(sess, x, y, model, x_test, y_test, attack_method = "fgsm", target = None, batch_size =128):
     tf.set_random_seed(1822)
     set_log_level(logging.DEBUG)
-    
+
     # Initialize attack
     if attack_method == "fgsm":
         from cleverhans.attacks import FastGradientMethod
@@ -161,7 +163,7 @@ def attack_classifier(sess, x, y, model, x_test, y_test, attack_method = "fgsm",
         if target is not None:
             params["y_target"] = tf.constant(np.repeat(np.eye(10)[target:target+1], batch_size, axis = 0))
         method = FastGradientMethod(model, sess=sess)
-        
+
     elif attack_method == "bim":
         from cleverhans.attacks import BasicIterativeMethod
         params = {'eps':8/255,
@@ -173,7 +175,7 @@ def attack_classifier(sess, x, y, model, x_test, y_test, attack_method = "fgsm",
         if target is not None:
             params["y_target"] = tf.constant(np.repeat(np.eye(10)[target:target+1], batch_size, axis = 0))
         method = BasicIterativeMethod(model,sess = sess)
-        
+
     elif attack_method == "jsma":
         from cleverhans.attacks import SaliencyMapMethod
         params = {'theta':8/255,
@@ -183,7 +185,7 @@ def attack_classifier(sess, x, y, model, x_test, y_test, attack_method = "fgsm",
                  }
         assert target is None
         method = SaliencyMapMethod(model,sess = sess)
-        
+
     elif attack_method == "cw":
         from cleverhans.attacks import CarliniWagnerL2
         params = {
@@ -200,7 +202,7 @@ def attack_classifier(sess, x, y, model, x_test, y_test, attack_method = "fgsm",
         if target is not None:
             params["y_target"] = tf.constant(np.repeat(np.eye(10)[target:target+1], batch_size, axis = 0))
         method = CarliniWagnerL2(model,sess = sess)
-    
+
     elif attack_method == "deepfool":
         from cleverhans.attacks import DeepFool
         params = {
@@ -213,11 +215,11 @@ def attack_classifier(sess, x, y, model, x_test, y_test, attack_method = "fgsm",
         }
         assert target is None
         method = DeepFool(model,sess = sess)
-        
+
     else:
         raise ValueError("Can not recognize this attack method")
-    
-    
+
+
     adv_x = method.generate(x, **params)
     num_batch = x_test.shape[0]// batch_size
     adv_imgs = []
@@ -233,28 +235,32 @@ def attack_classifier(sess, x, y, model, x_test, y_test, attack_method = "fgsm",
                 x:x_test[num_batch*batch_size:],
                 y:y_test[num_batch*batch_size:]
             }))
-        
+
     adv_imgs = np.concatenate(adv_imgs, axis = 0)
-        
+
     return adv_imgs
 
-def filter_data(sess, x, y, model, x_test, y_test, target = None, batch_size = 128, eval_size =  1280):
-    predictions = model.get_probs(x)
-    eval_par = {'batch_size': batch_size}
-    
+def filter_data(sess, x, y, model, x_test, y_test, target = None, batch_size = 128, eval_size =  1280, opposite=False):
+    pred = model.get_probs(x)
+    eval_single = tf.equal(tf.argmax(pred, axis=1), tf.argmax(y, axis=1))
+
     # only take the ones that are correctly classified by the target model
     x_filtered = []
     y_filtered = []
+    indices = []
     counter = 0
-    for each in zip(x_test,y_test):
-        if np.argmax(each[1]) != target and model_eval(sess, x, y, predictions,np.array([each[0]]),np.array([each[1]]),args = {'batch_size':1}) == 1.0:
-            x_filtered.append([each[0]])
-            y_filtered.append([each[1]])
+    for i in range(len(x_test)):
+        correct = sess.run(eval_single, feed_dict={x: [x_test[i]], y: [y_test[i]]})
+        if opposite: correct = not correct
+        if np.argmax(y_test[i]) != target and correct:
+            x_filtered.append([x_test[i]])
+            y_filtered.append([y_test[i]])
+            indices.append(i)
             counter += 1
-        if counter >= eval_size:
-            break
-    return np.concatenate(x_filtered,axis = 0), np.concatenate(y_filtered, axis = 0)
-    
+        if counter >= eval_size: break
+
+    return np.concatenate(x_filtered, axis=0), np.concatenate(y_filtered, axis=0), np.array(indices)
+
 def model_eval_adv(sess, x, y, model, x_eval, y_eval, target = None, batch_size = 128):
     preds_adv =  model.get_probs(x)
     eval_par = {'batch_size': batch_size}
@@ -283,7 +289,7 @@ def train_classifier(model_name, nb_epochs):
     # Define input TF placeholder
     x = tf.placeholder(tf.float32, shape=(None, 32, 32, 3))
     y = tf.placeholder(tf.float32, shape=(None, 10))
-    
+
     # Initialize model
     epoch_step = tf.Variable(0, trainable=False)
     if model_name == 'simple':
@@ -296,7 +302,7 @@ def train_classifier(model_name, nb_epochs):
         learning_rate = tf.train.exponential_decay(
             learning_rate,
             global_step=epoch_step,
-            decay_steps=nb_epochs, 
+            decay_steps=nb_epochs,
             decay_rate=0.9441,
             staircase=True)
         learning_rate = tf.case([
